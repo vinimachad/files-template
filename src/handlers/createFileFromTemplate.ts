6d520e321997d0410e516a83d7bdc3392a201058
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2023-02-24 08:57:41 -0400

    [UPDATE] Refactoring create file from template

--- src/handlers/createFileFromTemplate.ts
+++ src/handlers/createFileFromTemplate.ts
@@ -3,29 +3,12 @@ import { IConfiFile, ITemplate } from "../extension";
 import createScene from "./createScene/createScene";
 
 export default async (templatePath: string, currentPath: string, config: IConfiFile) => {
-    var templateOptions: string[] = [];
-    for (var key in config.templates) {
-        templateOptions.push(key);
-    }
     
-    let selectedOptionQuickPick = await vscode.window.showQuickPick(templateOptions, {
-        title: "Templates",
-        placeHolder: "Selecione o template desejado."
-    });
-
-    if (!selectedOptionQuickPick) {
-        return;
-    }
-
-    let selectedOption = config.templates[selectedOptionQuickPick] as unknown as ITemplate;
-    let sceneName = await vscode.window.showInputBox({
-        title: "Nome da cena",
-        placeHolder: "Escreva o nome da sua cena",
-    });
-
-    if (!sceneName) {
-        return;
-    }
+    const templateOptions = buildTemplateOptions();
+    const selectedOptionQuickPick = await getSelectedQuickPickOption();
+    const selectedOption = config.templates[selectedOptionQuickPick] as unknown as ITemplate;
+    const sceneName = await getSceneNameInput();
+    
     createScene({
         sceneName,
         selectedOption,
@@ -33,4 +16,38 @@ export default async (templatePath: string, currentPath: string, config: IConfiF
         destinationPath: currentPath,
         optionKind: selectedOptionQuickPick
     });
+
+    async function getSceneNameInput(): Promise<string> {
+        let sceneName = await vscode.window.showInputBox({
+            title: "Nome da cena",
+            placeHolder: "Escreva o nome da sua cena",
+        });
+    
+        if (!sceneName) {
+            return '';
+        }
+
+        return sceneName;
+    }
+
+    async function getSelectedQuickPickOption(): Promise<string> {
+        let selectedOptionQuickPick = await vscode.window.showQuickPick(templateOptions, {
+            title: "Templates",
+            placeHolder: "Selecione o template desejado."
+        });
+    
+        if (!selectedOptionQuickPick) {
+            return '';
+        }
+
+        return selectedOptionQuickPick;
+    }
+
+    function buildTemplateOptions(): string[] {
+        let options: string[] = [];
+        for (var key in config.templates) {
+            options.push(key);
+        }
+        return options;
+    }
 };
